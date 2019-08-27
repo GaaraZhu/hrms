2dc851f3d753aa5bd2004b0285d20610fe6dcc1c
sec.gary@gmail.com <local@g.com>
2019-08-27 22:21:23 +1200

    update schema.sql for job table

--- schema.sql
+++ schema.sql
@@ -36,7 +36,7 @@ CREATE TABLE `candidate` (
   `lastUpdatedBy` varchar(20) NOT NULL,
   `lastUpdatedTime` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='候选人';
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='候选人';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,6 +45,7 @@ CREATE TABLE `candidate` (
 
 LOCK TABLES `candidate` WRITE;
 /*!40000 ALTER TABLE `candidate` DISABLE KEYS */;
+INSERT INTO `candidate` VALUES (1,'刘力','MALE','12334564543','321283193009845','上海','浦东新区张江川北公路1号',2,'admin','2019-08-22 22:44:27','admin','2019-08-22 22:44:27'),(2,'张勇','MALE','123456722342','321283193009845','上海','浦东新区张江川北公路1号',1,'admin','2019-08-24 23:09:44','admin','2019-08-24 23:09:44');
 /*!40000 ALTER TABLE `candidate` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,7 +68,7 @@ CREATE TABLE `department` (
   `lastUpdatedBy` varchar(20) NOT NULL,
   `lastUpdatedTime` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='公司部门表';
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='公司部门表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,8 +77,45 @@ CREATE TABLE `department` (
 
 LOCK TABLES `department` WRITE;
 /*!40000 ALTER TABLE `department` DISABLE KEYS */;
-INSERT INTO `department` VALUES (1,'财务','001','赵飞','15685638702',1,'admin','2019-08-19 22:17:41','admin','2019-08-19 22:17:41'),(2,'销售','002','刘春燕','14565638701',0,'admin','2019-08-19 22:17:41','admin','2019-08-19 22:17:41'),(3,'后勤','003','刘勇','16265732724',0,'admin','2019-08-19 22:17:42','admin','2019-08-19 22:17:42'),(5,'研发','004','张力','0273697573',1,'admin','2019-08-20 22:39:59','admin','2019-08-20 22:39:59');
+INSERT INTO `department` VALUES (1,'财务','001','赵飞','15685638704',3,'admin','2019-08-19 22:17:41','admin','2019-08-19 22:17:41'),(2,'销售','002','刘春燕','14565638701',0,'admin','2019-08-19 22:17:41','admin','2019-08-19 22:17:41'),(3,'后勤','003','刘勇','16265732724',0,'admin','2019-08-19 22:17:42','admin','2019-08-19 22:17:42'),(5,'研发','004','张力','0273697573',1,'admin','2019-08-20 22:39:59','admin','2019-08-20 22:39:59');
 /*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `job`
+--
+
+DROP TABLE IF EXISTS `job`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company` varchar(50) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `salaryRange` varchar(50) NOT NULL,
+  `quota` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `referralBonus` tinyint(1) NOT NULL DEFAULT '1',
+  `referralBonusAmount` int(11) NOT NULL,
+  `note` varchar(20) NOT NULL,
+  `jpaVersion` int(11) NOT NULL,
+  `createdBy` varchar(20) NOT NULL,
+  `createdTime` datetime NOT NULL,
+  `lastUpdatedBy` varchar(20) NOT NULL,
+  `lastUpdatedTime` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='企业职位表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job`
+--
+
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -120,4 +158,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-08-21 23:52:14
+-- Dump completed on 2019-08-27 22:20:22
