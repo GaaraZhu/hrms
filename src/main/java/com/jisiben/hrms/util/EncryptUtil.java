c48a9faa19aa0d64224212d22159901fe48b8679
sec.gary@gmail.com <local@g.com>
2019-09-11 19:33:26 +1200

    update encrypt utilities

--- src/main/java/com/jisiben/hrms/util/EncryptUtil.java
+++ src/main/java/com/jisiben/hrms/util/EncryptUtil.java
@@ -6,16 +6,16 @@ import org.springframework.security.authentication.encoding.PasswordEncoder;
 public class EncryptUtil {
     private static final PasswordEncoder encoder = new Md5PasswordEncoder();
 
-    public static String encrypt(String rawPassword) {
-        return encoder.encodePassword(rawPassword, "aaa");
+    public static String encrypt(String presentedPassword, String userName) {
+        return encoder.encodePassword(presentedPassword, userName);
     }
 
-    public static boolean match(String rawPassword, String password) {
-        return encoder.isPasswordValid(rawPassword, password, "admin");
+    public static boolean match(String rawPassword, String password, String userName) {
+        return encoder.isPasswordValid(rawPassword, password, userName);
     }
 
     public static void main(String[] args) {
-        System.out.println(encrypt("123"));
-        System.out.println(match("86888061b399e74e30eeead8c7aab922", "123"));
+        System.out.println(encrypt("123", "gary"));
+        System.out.println(match("010787e658be76708b40aa336e482293", "123", "gary"));
     }
 }
