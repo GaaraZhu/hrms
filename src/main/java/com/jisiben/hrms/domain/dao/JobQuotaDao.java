0ff15868377c8a23dc950faa02d2082024042609
sec.gary@gmail.com <local@g.com>
2019-11-17 23:19:59 +1300

    adds look up dao method for job quota

--- src/main/java/com/jisiben/hrms/domain/dao/JobQuotaDao.java
+++ src/main/java/com/jisiben/hrms/domain/dao/JobQuotaDao.java
@@ -2,6 +2,17 @@ package com.jisiben.hrms.domain.dao;
 
 import com.jisiben.hrms.domain.dao.common.Dao;
 import com.jisiben.hrms.domain.entity.JobQuota;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 public interface JobQuotaDao extends Dao<JobQuota, Long> {
+    @Query("FROM JobQuota jq WHERE (:company is null or jq.job.company = :company) and (:city is null or jq.job.city = :city)"
+            + "and (:jobName is null or jq.job.name like CONCAT('%',:jobName,'%')) and (:month is null or jq.month = :month)")
+    Page<JobQuota> findJobQuotas(
+            @Param("company")String company,
+            @Param("city")String city,
+            @Param("jobName")String jobName,
+            @Param("month")String month,
+            Pageable pageable);
 }
--- src/main/java/com/jisiben/hrms/service/impl/JobQuotaServiceImpl.java
+++ src/main/java/com/jisiben/hrms/service/impl/JobQuotaServiceImpl.java
@@ -1,13 +1,13 @@
 package com.jisiben.hrms.service.impl;
 
 import com.jisiben.hrms.domain.dao.JobQuotaDao;
-import com.jisiben.hrms.domain.dao.UserDao;
 import com.jisiben.hrms.domain.dao.common.Dao;
 import com.jisiben.hrms.domain.entity.JobQuota;
 import com.jisiben.hrms.service.JobQuotaService;
 import com.jisiben.hrms.service.common.impl.AbstractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,18 +18,19 @@ import java.util.Optional;
 @Transactional
 public class JobQuotaServiceImpl extends AbstractService<JobQuota> implements JobQuotaService {
     @Autowired
-    private JobQuotaDao JobQuotaDao;
-
-    @Autowired
-    private UserDao userDao;
+    private JobQuotaDao jobQuotaDao;
 
     @Override
     protected Dao<JobQuota, Long> getDao() {
-        return JobQuotaDao;
+        return jobQuotaDao;
     }
 
     @Override
     public Page<JobQuota> search(Map<String, Optional<Object>> criteria, int currentPage, int pageSize) {
-        return null;
+        String company = criteria.get("company").map(Object::toString).orElse(null);
+        String city = criteria.get("city").map(Object::toString).orElse(null);
+        String jobName = criteria.get("jobName").map(Object::toString).orElse(null);
+        String month = criteria.get("month").map(Object::toString).orElse(null);
+        return jobQuotaDao.findJobQuotas(company, city, jobName, month, new PageRequest(currentPage - 1, pageSize));
     }
 }
