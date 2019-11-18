1c39e06426441e319ce38a9f2051956b1a163008
sec.gary@gmail.com <local@g.com>
2019-11-18 20:09:08 +1300

    checks whether jobQuota exists or not before creating

--- src/main/webapp/WEB-ROOT/js/jobQuota.js
+++ src/main/webapp/WEB-ROOT/js/jobQuota.js
@@ -118,59 +118,88 @@
 //    }
 //}
 
-$('#jobQuotaForm').bootstrapValidator({
-    message : 'This value is not valid',
-    feedbackIcons : {
-        valid : 'glyphicon glyphicon-ok',
-        invalid : 'glyphicon glyphicon-remove',
-        validating : 'glyphicon glyphicon-refresh'
-    },
-    fields : {
-        quota : {
-            validators : {
-                notEmpty : {
-                    message : '指标不能为空'
+    $('#month').change(function() {
+        wto = setTimeout(function() {
+            jobId = $("#jobId").val();
+            month = $("#month").val();
+            $.ajax({
+                url : "jobQuotaCount",
+                type : "GET",
+                async: true,
+                data : "jobId=" + jobId + "&month=" + month,
+                contentType : "application/json;charset=utf-8",
+                success : function(count) {
+                    if (count> 0) {
+                        $("#jobQuotaAlertModal").load( "WEB-ROOT/html/common/alert.jsp", function( response, status, xhr ) {
+                            $("#alertText").text("该月指标已创建，请去每月指标页面查看");
+                            $("#alertModel").modal({
+                                keyboard: true
+                            });
+                        });
+                    }
                 },
-                integer: {
-                    message: '指标格式错误'
-                },
-            }
-        },
-    }
-}).on(
-    'success.form.bv',
-    onCreateOrUpdateJobQuota
-);
-
-function onCreateOrUpdateJobQuota(e){
-    e.preventDefault();
-    var data = $('form#jobQuotaForm').serializeObject();
-    data.jobId = $('#jobId').val();
-    var method = $("#submitType").val();
-    var url = method=="POST"?"jobQuota?id="+$("#id").val():"jobQuota";
-    $.ajax({
-        type : method,
-        url : url,
-        data: JSON.stringify(data),
-        async: true,
-        contentType: 'application/json;charset=utf-8',
-        success : function() {
-             $("#jobQuotaAlertModal").load( "WEB-ROOT/html/common/alert.jsp", function( response, status, xhr ) {
-                $("#alertText").text("操作成功");
-                $("#alertModel").modal({
-                    keyboard: true
-                });
-             });
-             queryUsers(1);
-        },
-        error : function(msg) {
-            $("#jobQuotaAlertModal").load( "WEB-ROOT/html/common/alert.jsp", function( response, status, xhr ) {
-                $("#alertText").text("操作失败");
-                $("#alertModel").modal({
-                    keyboard: true
-                });
+                error : function(e) {
+                    if (e.status != 401) {
+                        console.log(e);
+                    }
+                }
             });
-        }
+        }, 1000);
     });
-}
+
+    $('#jobQuotaForm').bootstrapValidator({
+        message : 'This value is not valid',
+        feedbackIcons : {
+            valid : 'glyphicon glyphicon-ok',
+            invalid : 'glyphicon glyphicon-remove',
+            validating : 'glyphicon glyphicon-refresh'
+        },
+        fields : {
+            quota : {
+                validators : {
+                    notEmpty : {
+                        message : '指标不能为空'
+                    },
+                    integer: {
+                        message: '指标格式错误'
+                    },
+                }
+            },
+        }
+    }).on(
+        'success.form.bv',
+        onCreateOrUpdateJobQuota
+    );
+
+    function onCreateOrUpdateJobQuota(e){
+        e.preventDefault();
+        var data = $('form#jobQuotaForm').serializeObject();
+        data.jobId = $('#jobId').val();
+        var method = $("#submitType").val();
+        var url = method=="POST"?"jobQuota?id="+$("#id").val():"jobQuota";
+        $.ajax({
+            type : method,
+            url : url,
+            data: JSON.stringify(data),
+            async: true,
+            contentType: 'application/json;charset=utf-8',
+            success : function() {
+                 $("#jobQuotaAlertModal").load( "WEB-ROOT/html/common/alert.jsp", function( response, status, xhr ) {
+                    $("#alertText").text("操作成功");
+                    $("#alertModel").modal({
+                        keyboard: true
+                    });
+                 });
+                 queryUsers(1);
+            },
+            error : function(msg) {
+                $("#jobQuotaAlertModal").load( "WEB-ROOT/html/common/alert.jsp", function( response, status, xhr ) {
+                    $("#alertText").text("操作失败");
+                    $("#alertModel").modal({
+                        keyboard: true
+                    });
+                });
+            }
+        });
+    }
 
