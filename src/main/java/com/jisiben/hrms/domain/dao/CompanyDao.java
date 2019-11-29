a9e899dd37738f238c84b30b4f5b34e429f15c0b
sec.gary@gmail.com <local@g.com>
2019-11-29 22:44:25 +1300

    support wildcard search for company

--- src/main/java/com/jisiben/hrms/domain/dao/CompanyDao.java
+++ src/main/java/com/jisiben/hrms/domain/dao/CompanyDao.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface CompanyDao extends Dao<Company, Long> {
-    @Query("FROM Company c WHERE (:name is null or c.name = :name) and (:city is null"
+    @Query("FROM Company c WHERE (:name is null or c.name like CONCAT('%',:name,'%')) and (:city is null"
             + " or c.city = :city)")
     Page<Company> findByNameAndCity(@Param("name")String name, @Param("city")String city, Pageable pageable);
 }
