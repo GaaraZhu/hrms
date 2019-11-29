e05419c980b0d72b61d0b537aafb0a87e1931694
sec.gary@gmail.com <local@g.com>
2019-11-29 22:45:03 +1300

    support wildcard search for branch

--- src/main/java/com/jisiben/hrms/domain/dao/BranchDao.java
+++ src/main/java/com/jisiben/hrms/domain/dao/BranchDao.java
@@ -7,8 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface BranchDao extends Dao<Branch, Long> {
-    @Query("FROM Branch c WHERE (:company is null or c.company.name = :company) and (:branch is null"
-            + " or c.name = :branch)")
+    @Query("FROM Branch c WHERE (:company is null or c.company.name like CONCAT('%',:company,'%')) and (:branch is null"
+            + " or c.name like CONCAT('%',:branch,'%'))")
     Page<Branch> findByManagerAndName(@Param("company")String company, @Param("branch")String branch, Pageable pageable);
+
 }
